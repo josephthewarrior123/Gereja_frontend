8cce9e649e69a35a30f2a26680eea5bfb5b08961
joseph imanuel setiawan <128193636+josephthewarrior1@users.noreply.github.com>
2025-07-19 14:04:10 +0700

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,15 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/', // Always use root for Vercel
-  server: {
-    port: 9875,
-    open: true
-  },
-  css: {
-    modules: {
-      localsConvention: 'camelCase'
-    }
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true
-  }
+    plugins: [react(), tailwindcss()],
+    server: {
+        port: 9875,
+        cors: false,
+    },
+    optimizeDeps: {
+        exclude: ['js-big-decimal'],
+    },
 });
